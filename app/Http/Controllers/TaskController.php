0023011193b23437836e745ea9c1da698772c5ed
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-12-15 20:24:51 +0800

    新增TaskController相關方法：index、store、destroy

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    #限制只有登入的user可進入TaskController底下的路由
+    public function __construct()
+    {
+        $this -> middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,
+        [
+            #任務名稱為必填欄位，且字數不可超過255
+            'name' => 'required|max:255',
+        ]);
+    }
+
+    public function destroy(Request $request,Task $task)
+    {
+        $this->authorize('destroy',$task);
+        $task->delete();
+        return redirect('/tasks');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 #顯示所有任務
-Route::get('/tasks',[\App\Http\Controllers\TaskController::class],'index') ->name('task.index');
+Route::get('/tasks',[\App\Http\Controllers\TaskController::class],'index') ->name('tasks.index');
 #儲存任務
 Route::post('/task',[\App\Http\Controllers\TaskController::class],'store') ->name('task.store');
 #刪除任務
