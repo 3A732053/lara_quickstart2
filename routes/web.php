3bac85b8265c00f049c00cf72ca68306a647b585
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-12-15 19:48:19 +0800

    新增task相關路由：index、store、destroy

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,10 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+#顯示所有任務
+Route::get('/tasks',[\App\Http\Controllers\TaskController::class],'index') ->name('task.index');
+#儲存任務
+Route::post('/task',[\App\Http\Controllers\TaskController::class],'store') ->name('task.store');
+#刪除任務
+Route::delete('/task/{task}',[\App\Http\Controllers\TaskController::class],'destroy') ->name('task.destroy');
